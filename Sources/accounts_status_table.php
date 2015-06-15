4f503c440cdf4db9dcf3763236b6aee9ec3ef474
Sergey Maruda <maruda@ukr.net>
2015-06-15 16:54:56 +0300

    making correct html layout

--- Sources/accounts_status_table.php
+++ Sources/accounts_status_table.php
@@ -3,31 +3,27 @@
     <meta charset="utf-8">
     <title>Состояние счетов</title> 
     <link rel="stylesheet" type="text/css" href="mystyle.css">
-  </head>
   
+    <style>
+     .layer1 
+       {
+       background: #EAF2D3;
+       border: 1px solid black;
+       padding: 5px;
+      }
+    </style>
+  </head>
+
   <body>
- 
+    <div >
+      <a class="layer1" href="manage_transactions.php">Manage transaction</a>
+      <a class="layer1" href="transactions_view.php">View transaction</a>
+      <a class="layer1" href="accounts_status_by_currency_table.php">Accounts state by currency</a>
+      <a class="layer1" href="accounts_status_table.php">Accounts status</a>
+    </div>
 
-  <style>
-   .layer1 {
-    background: #EAF2D3;
-    border: 1px solid black;
-    padding: 5px;
-   }
-  </style>
+    <br>
 
-</head>
-<body>
-
-  <div >
-    <a class="layer1" href="manage_transactions.php">Manage transaction</a>
-    <a class="layer1" href="transactions_view.php">View transaction</a>
-    <a class="layer1" href="accounts_status_by_currency_table.php">Accounts state by currency</a>
-  	<a class="layer1" href="accounts_status_table.php">Accounts status</a>
-  </div>
-
-  <br>
-    
     <?php include 'accounts_status.php';?>
   </body>
 </html> 
