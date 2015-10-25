30b8b96c4782221d679686802344212cdce7d04d
Sergey <maruda@ukr.net>
2015-10-25 20:37:42 +0200

    minor changes

--- index.php
+++ index.php
@@ -3,22 +3,21 @@ include('header.php');
 ?>
 
 <div class="leftbar">
-<?php
-include('accounts_status.php');
-?>
+  <?php
+  include('accounts_status.php');
+  ?>
 </div >
   
- <div >
-
-<?php
-include('accounts_status_by_currency.php');
-?>
-  </div >
+<div >
+  <?php
+  include('accounts_status_by_currency.php');
+  ?>
+</div >
 
  <div class="noleft">
-<?php
-include('transactions.php');
-?>
-  </div >
+  <?php
+  include('transactions.php');
+  ?>
+</div >
 </body>
 </html>
